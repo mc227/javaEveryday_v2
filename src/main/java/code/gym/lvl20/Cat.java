b91aab5d3dbe978a50728eed5c834a7073c7cad3
mc227 <markcostales.mark@gmail.com>
2024-01-20 18:35:34 -0500

    getting ready for tomorrow. I zeroed out Cat, Dog and Human  class

--- src/main/java/code/gym/lvl20/Cat.java
+++ src/main/java/code/gym/lvl20/Cat.java
@@ -1,47 +1,4 @@
 package code.gym.lvl20;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.PrintWriter;
-
 public class Cat {
-    public String name;
-    public int age;
-    public int weight;
-
-    public void save(PrintWriter writer) throws Exception {
-        writer.println(name);
-        writer.println(age);
-        writer.println(weight);
-        writer.flush();
-    }
-
-    public void load(BufferedReader reader) throws Exception {
-        name = reader.readLine();
-        age = Integer.parseInt(reader.readLine());
-        weight = Integer.parseInt(reader.readLine());
-    }
-
-
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.name = "Mark";
-        cat.age = 39;
-        cat.weight = 200;
-        try (PrintWriter writer = new PrintWriter("mark.txt")){
-            cat.save(writer);
-            System.out.println("Successfully saved cat info!!!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        try (BufferedReader reader =  new BufferedReader(new FileReader("mark.txt"))){
-            cat.load(reader);
-            System.out.println(cat.name);
-            System.out.println(cat.age);
-            System.out.println(cat.weight);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
