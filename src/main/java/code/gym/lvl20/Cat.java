c51a8dcda481cacaec694c77f5f4078db7e4909a
mc227 <markcostales.mark@gmail.com>
2024-01-22 01:52:38 -0500

    strong and independent, i don't need no other java developer

--- src/main/java/code/gym/lvl20/Cat.java
+++ src/main/java/code/gym/lvl20/Cat.java
@@ -1,4 +1,24 @@
 package code.gym.lvl20;
 
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class Cat {
+    public String name;
+    public int age;
+    public int weight;
+
+    public void save(PrintWriter writer) throws Exception{
+        writer.println(name);
+        writer.println(age);
+        writer.println(weight);
+        writer.flush();
+    }
+
+    public void load(BufferedReader reader) throws Exception{
+        name = reader.readLine();
+        age = Integer.parseInt(reader.readLine());
+        weight = Integer.parseInt(reader.readLine());
+    }
 }
