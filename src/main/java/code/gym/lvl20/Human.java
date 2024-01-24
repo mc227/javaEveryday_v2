660153d08af4effe4b86607f3b7a236dbd309b32
mc227 <markcostales.mark@gmail.com>
2024-01-24 13:54:24 -0500

    wrote from scratch the the human intermediate implementation

--- src/main/java/code/gym/lvl20/Human.java
+++ src/main/java/code/gym/lvl20/Human.java
@@ -1,2 +1,21 @@
-package code.gym.lvl20;public class Human {
+package code.gym.lvl20;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class Human {
+    public Cat cat;
+    public Dog dog;
+
+    public void save(PrintWriter writer) throws Exception {
+        cat.save(writer);
+        dog.save(writer);
+    }
+
+    public void load(BufferedReader reader) throws Exception {
+        cat = new Cat();
+        cat.load(reader);
+        dog = new Dog();
+        dog.load(reader);
+    }
 }
