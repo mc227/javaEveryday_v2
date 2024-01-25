7cece1a35b4704d448354e95db38ae49a26fb09b
mc227 <markcostales.mark@gmail.com>
2024-01-25 00:27:59 -0500

    i'm really surprised that my solution worked

--- src/main/java/code/gym/lvl20/Human.java
+++ src/main/java/code/gym/lvl20/Human.java
@@ -12,26 +12,23 @@ public class Human {
         String isCatPresent = cat != null ? "yes":"no";
         writer.println(isCatPresent);
         writer.flush();
-
-        if(cat!=null)
+        if (cat != null)
             cat.save(writer);
-
         String isDogPresent = dog != null ? "yes":"no";
         writer.println(isDogPresent);
         writer.flush();
-
-        if(dog!=null)
+        if(dog != null)
             dog.save(writer);
     }
 
-    public void load(BufferedReader reader) throws Exception{
+    public void load(BufferedReader reader) throws Exception {
         String isCatPresent = reader.readLine();
         if(isCatPresent.equals("yes")){
             cat = new Cat();
             cat.load(reader);
         }
         String isDogPresent = reader.readLine();
-        if(isDogPresent.equals("yes")){
+        if (isDogPresent.equals("yes")){
             dog = new Dog();
             dog.load(reader);
         }
