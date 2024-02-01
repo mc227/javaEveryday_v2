113c94a05388007ca3f3148492c71017cfee62d1
mc227 <markcostales.mark@gmail.com>
2024-02-01 09:58:51 -0500

    wrote from scratch Cat Externalizable class

--- src/main/java/code/gym/java/course/java/core/save/data/Externalizable/Cat.java
+++ src/main/java/code/gym/java/course/java/core/save/data/Externalizable/Cat.java
@@ -1,4 +1,26 @@
 package code.gym.java.course.java.core.save.data.Externalizable;
 
-public class Cat {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Cat implements Externalizable {
+    public String name;
+    public int age;
+    public int weight;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(this.name);
+        out.writeObject(this.age);
+        out.writeObject(this.weight);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        name = (String) in.readObject();
+        age = (int) in.readObject();
+        weight = (int) in.readObject();
+    }
 }
