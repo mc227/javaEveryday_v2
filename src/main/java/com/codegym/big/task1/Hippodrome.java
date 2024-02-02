4df61cc5fb149721952cfa43b19059cfbb971da6
mc227 <markcostales.mark@gmail.com>
2024-02-02 01:25:14 -0500

    part2

--- src/main/java/com/codegym/big/task1/Hippodrome.java
+++ src/main/java/com/codegym/big/task1/Hippodrome.java
@@ -1,4 +1,15 @@
 package com.codegym.big.task1;
 
+import java.util.List;
+
 public class Hippodrome {
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
